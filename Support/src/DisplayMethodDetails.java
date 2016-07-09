44c76774b7386e446792a13296ea7e87f29e97f3
Eric Dunbar <eric.dunbar@gmail.com>
2016-07-09 18:37:47 -0400

    a

--- Support/src/DisplayMethodDetails.java
+++ Support/src/DisplayMethodDetails.java
@@ -1,33 +1,38 @@
-
-public class TestingSupport {
-	private static boolean isTesting = false;
+/**
+ * Displays information about the current method.
+ * 
+ * @author Eric Dunbar
+ *
+ */
+public class DisplayMethodDetails {
+	private static boolean testing = false;
 	public static int MICounter = 0;
-	
-	public static void resetCounter(){
+
+	public static void resetCounter() {
 		MICounter = 0;
 	}
 
 	public static boolean isTesting() {
-		return isTesting;
+		return testing;
 	}
 
 	public static void setTesting(boolean isTesting) {
-		TestingSupport.isTesting = isTesting;
+		testing = isTesting;
 	}
 
 	public static boolean getTesting() {
-		return TestingSupport.isTesting;
+		return testing;
 	}
 
 	/**
-	 * Method info designed for TwoQueueStack
+	 * Method info modified for TwoQueueStack
 	 * 
 	 * @param methodDetail
 	 * @param counter
 	 */
 	public static void TQSMethodInfo(String methodDetail) {
 		// http://stackoverflow.com/questions/421280/how-do-i-find-the-caller-of-a-method-using-stacktrace-or-reflection
-		if (isTesting) {
+		if (testing) {
 			StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
 			System.out.println("   # " + ++MICounter + " #   "
 					+ stackTraceElements[2].getMethodName() + " " + methodDetail);
@@ -36,15 +41,9 @@ public class TestingSupport {
 
 	public static void methodInfo(String methodDetail) {
 		// http://stackoverflow.com/questions/421280/how-do-i-find-the-caller-of-a-method-using-stacktrace-or-reflection
-		if (isTesting) {
+		if (testing) {
 			StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
 			System.out.println("   " + stackTraceElements[2].getMethodName() + " " + methodDetail);
 		}
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
